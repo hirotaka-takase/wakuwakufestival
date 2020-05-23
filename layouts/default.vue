de940b6bd67315775a62f06499c6080d1431cac1
HIRO <hirotaka5315@gmail.com>
2020-05-23 09:41:12 +0900

    footerの土台を作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,8 +34,40 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer color="warning" class="lighten-1" padless>
+      <v-row class="py-3 white--text text-center" no-gutters>
+        <v-col cols="12">
+          <v-btn
+          v-for="link in footerLinks"
+          :key="link.name"
+          color="white"
+          text
+          :to="link.to"
+          nuxt
+          class="subtitle-2 font-weight-bold"
+          rounded>
+          {{ link.name }}
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+          v-for="icon in footerIcons"
+          :key="icon.name"
+          :href="icon.to"
+          target="_blank"
+          class="mx-4"
+          icon>
+            <v-icon size="27px">{{ icon.name }}</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="my-4 body-2">
+          わくわくフェスティバル運営企画部<br>
+          TEl: 058-263-5577 （平日 9:00〜18:00)
+        </v-col>
+        <v-col class="body-2" cols="12">
+          Copyright © 2020 - STYLE CREATION INC.
+        </v-col>
+      </v-row>
     </v-footer>
   </v-app>
 </template>
@@ -72,6 +104,34 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'お問い合わせ',
           to: '/contact'
+        }
+      ],
+      footerLinks: [
+        {
+          name: 'Home', 
+          to: '/'
+        },
+        {
+          name: '会社概要', 
+          to: '/company'
+        },
+        {
+          name: '前売り券コンビニで販売中！', 
+          to: '/ticket'
+        },
+      ],
+      footerIcons: [
+        {
+          name: 'mdi-facebook',
+          to: 'https://www.facebook.com/wakuwaku.fs'
+        },
+        {
+          name: 'mdi-twitter',
+          to: 'https://www.instagram.com/wakuwakufestival/'
+        },
+        {
+          name: 'mdi-instagram',
+          to: 'https://twitter.com/waku_fes'
         }
       ],
       miniVariant: false,
